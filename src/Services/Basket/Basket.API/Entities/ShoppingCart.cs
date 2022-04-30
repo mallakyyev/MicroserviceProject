381ecc1495dae99d00ad5a8abac93fef174fcffe
Максат Аллакыев <mdallakiev@gmail.com>
2022-04-30 12:24:38 +0500

    BasketAPIDOne

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; }
+        public List<ShoppingCartItems> Items{ get; set; }
+        public ShoppingCart()
+        {
+            Items = new List<ShoppingCartItems>();
+        }
+
+        public ShoppingCart(string userName)
+        {
+            Items = new List<ShoppingCartItems>();
+            UserName = userName;
+        }
+
+        public decimal TotalPrice {
+            get
+            {
+                decimal totalPrice = 0;
+                foreach (var item in Items)
+                    totalPrice += item.Price*item.Quantity;
+                return totalPrice;
+            }
+        }
+    }
+}
